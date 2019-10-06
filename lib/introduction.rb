ea3302f2e8fe37cd7bb601ea7ab22cc5060d92c4
Michael Z <mziacm@gmail.com>
2019-10-06 22:43:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language)
-  puts "Hi"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
